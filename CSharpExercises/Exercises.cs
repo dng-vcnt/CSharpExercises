fb940138c1f225b2e633449b384abdad7a465290
Vincent Duong <vincentduong@Vincents-MacBook-Pro.local>
2016-08-23 14:37:03 -0700

    Initial Commit

---
+++ CSharpExercises/Exercises.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises
+{
+    public static class Exercises
+    {
+        // Example Exercise:
+        // Create a method called HelloWorld that returns the following string - "Hello World!"
+        public static string HelloWorld()
+        {
+            return "Hello World!";
+        }
+
+        /* Alright - your turn now! */
+
+        // 1. Create a method called SayHello that accepts a string representing a name and returns a welcome message (E.g. "Hello John!")
+        public static string SayHello (string name)
+        {
+            string message = "Hello " + name + "!";
+            return message;
+        }
+        // 2. Create a method called Sum that accepts two integers and returns their sum.
+        public static int Sum (int val1, int val2)
+        {
+            int sum = val1 + val2;
+            return sum;
+        }
+        // 3. Create a method called Divide that accepts two decimals and returns the result of dividing the two numbers as a decimal.
+        public static decimal Divide (decimal val1, decimal val2)
+        {
+            decimal quotient = val1 / val2;
+            return quotient;
+        }
+        // 4. Create a method called CanOpenCheckingAccount that accepts an integer representing a customers age, returning true if the age is greater than or equal to 18, or false if the argument is less than 18.
+        public static bool CanOpenCheckingAccount (int age)
+        {
+            if (age >= 18)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+        // 5. Create a method called GetFirstName that accepts a string representing a full name (e.g. "John Smith"), and returns only the first name.
+        public static string GetFirstName (string fullName)
+        {
+            string[] name = fullName.Split(' ');
+            return name[0];
+        }
+        // 6. Create a method called ReverseStringHard that accepts a string and returns the string in reverse. (No built in functions allowed)
+        public static string ReverseStringHard (string name)
+        {
+            char[] nameStr = name.ToCharArray();
+            string reverseName = String.Empty;
+            for (int i = nameStr.Length - 1; i >= 0; --i)
+            {
+                reverseName += nameStr[i];
+            }
+
+            return reverseName;
+        }
+        // 7. Create a method called ReverseStringEasy that accepts a string and returns the string in reverse. (Using only built in functions)
+        public static string ReverseStringEasy (string name)
+        {
+            char[] nameStr = name.ToCharArray();
+            Array.Reverse(nameStr);
+            return new string(nameStr);
+        }
+        // 8. Create a method called PrintTimesTable that accepts an integer and returns the times table as a string for that number up to the 10th multiplication.
+        /* e.g. 10 should return
+         * 10 * 1 = 10
+         * 10 * 2 = 20
+         * 10 * 3 = 30
+         * 10 * 4 = 40
+         * 10 * 5 = 50
+         * 10 * 6 = 60
+         * 10 * 7 = 70
+         * 10 * 8 = 80
+         * 10 * 9 = 90
+         * 10 * 10 = 10 */
+        public static string PrintTimesTable (int integer)
+        {
+            int[] ans = new int[10];
+            string timesTable = "";
+            for (int i = 0; i < 10; i++)
+            {
+                ans[i] = integer * (i + 1);
+                if (i == 9)
+                {
+                    timesTable += integer + " * " + (i + 1) + " = " + ans[i];
+                }
+                else
+                {
+                    timesTable += integer + " * " + (i + 1) + " = " + ans[i] + "\r\n";
+                }
+
+            }
+            return timesTable;
+        }
+        // 9. Create a method called ConvertKelvinToFahrenheit that accepts a double representing a temperature in kelvin and returns a double containing the temperature in Fahrenheit.
+        public static double ConvertKelvinToFahrenheit (double tempK)
+        {
+            double tempF = ((tempK * 9)/5) - 459.67;
+            return Math.Round(tempF, 2);
+        }
+        // 10. Create a method called GetAverageHard that accepts an array of integers and returns the average value as a double. (No built in functions allowed)
+        public static double GetAverageHard (int[] intArray)
+        {
+            int sum = 0;
+            double average = 0;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                sum += intArray[i];
+            }
+            average = ((double)sum) / intArray.Length;
+            return average;
+        }
+        // 11. Create a method called GetAverageEasy that accepts an array of integers and returns the average value as a double. (Using only built in functions)
+        public static double GetAverageEasy(int[] intArray)
+        {
+            double average = 0;
+            average = intArray.Average();
+            return average;
+        }
+        // 12. Create a method called DrawTriangle that accepts two integers - number and width and returns a string containing a drawn triangle using the number parameter.
+        /* e.g. Number: 8, Width: 8 should return
+         * 88888888
+         * 8888888
+         * 888888
+         * 88888
+         * 8888
+         * 888
+         * 88
+         * 8 */
+        public static string DrawTriangle (int number, int width)
+        {
+            string triangle = "";
+            int widthHorizontal = width;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = widthHorizontal; j > 0; j--)
+                {
+                    triangle += number.ToString();
+                }
+                if (i < width - 1)
+                {
+                    triangle += "\r\n";
+                }
+                widthHorizontal--;
+            }
+            return triangle;
+        }
+        // 13. Create a method called GetMilesPerHour that accepts a double representing distance and three integers representing hours, minutes and seconds. The method should return the speed in MPH rounded to the nearest whole number as a string. (e.g. "55MPH")
+        public static string GetMilesPerHour (double dist, int hours, int mins, int sec)
+        {
+            int timeInSec = 0;
+            double timeInHour = 0;
+            double milesPerHour;
+            string speed;
+
+            timeInSec = sec + (mins * 60) + (hours * 3600);
+            timeInHour = timeInSec / 3600.0;
+            milesPerHour = dist / timeInHour;
+            milesPerHour = Math.Round(milesPerHour, 0);
+            speed = milesPerHour.ToString() + "MPH";
+            return speed;
+            
+        }
+
+        // 14. Create a method called IsVowel that accepts a char parameter and returns true if the parameter is a vowel or false if the parameter is a consonant.
+        public static bool IsVowel (char c)
+        {
+            if (c == 'a')
+            {
+                return true;
+            }
+            else if (c == 'e')
+            {
+                return true;
+            }
+            else if (c == 'i')
+            {
+                return true;
+            }
+            else if (c == 'o')
+            {
+                return true;
+            }
+            else if (c == 'u')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        // 15. Create a method called IsConsonant that accepts a char parameter and returns true if the parameter is a consonant or false if the parameter is a vowel.
+        public static bool IsConsonant (char c)
+        {
+            if (c == 'a')
+            {
+                return false;
+            }
+            else if (c == 'e')
+            {
+                return false;
+            }
+            else if (c == 'i')
+            {
+                return false;
+            }
+            else if (c == 'o')
+            {
+                return false;
+            }
+            else if (c == 'u')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        // 16. The Collatz conjecture, named after Lothar Collatz of Germany, proposed the following conjecture in 1937. 
+        // Beginning with an integer n > 1, repeat the following until n == 1. If n is even, halve it. If n is odd, triple it and add 1. Following these steps, the function will always arrive at the number 1.
+        // Create a method called CollatzConjecture that accepts an integer and returns the number of steps required to get to n == 1 as an integer.
+        public static int CollatzConjecture (int integer)
+        {
+            int i = 0;
+            while (integer != 1) 
+            {
+                if (integer % 2 == 0)
+                {
+                    integer = integer / 2;
+                }
+                else
+                {
+                    integer = (integer * 3) + 1;
+                }
+                i++;
+            }
+            return i;
+        }
+
+        //16.5 create a method called GetOccurences that accepts two strings. The first string should represent a string of text, and the other should represent a single word. GetOccurrences should return the number of times that the given word appears in the string of text.
+        public static int GetOccurences(string text, string word)
+        {
+            string[] wordArg = { word };
+            string[] words = text.Split(wordArg, StringSplitOptions.None);
+            return words.Length - 1;
+        }
+
+        // 17. Create a method called GetNext7Days that accepts a DateTime object and returns an array of DateTime objects containing the next 7 days (including the given day).
+        public static DateTime[] GetNext7Days (DateTime date)
+        {
+            DateTime[] next7Days = new DateTime[7];
+            for (int i = 0; i < 7; i++)
+            {
+                next7Days[i] = date.AddDays(i);
+            }
+            return next7Days;
+        }
+
+        // 18. Create a method called IsInLeapYear that accepts a DateTime object and returns true if the date falls within a leap year and false if not. (No built in functions allowed)
+        public static bool IsInLeapYear (DateTime date)
+        {
+            return DateTime.DaysInMonth(date.Year, 2) == 29;
+        }
+
+        // 19. Create a method called MortgageCalculator that accepts 2 decimals representing loan balance and interest rate, an integer representing loan term in years, and an integer representing the payment period.
+        /* Payment periods: 1 - Monthly, 2 - Bi-Monthly (Every 2 months) */
+        public static decimal MortgageCalculator (decimal loanBalance, decimal interestRate, int loanTerm, int period)
+        {
+            int numOfPayments = loanTerm * period;
+            decimal monthlyInterestRate = (interestRate / 100) / period;
+            decimal compoundedInterestRate = (decimal)Math.Pow((double)(1 + monthlyInterestRate), numOfPayments);
+            decimal interestQuotient = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);
+            decimal monthlyPayment = Math.Round((loanBalance * interestQuotient) * 100) / 100;
+
+            return Math.Round(monthlyPayment, 2);
+        }
+
+        // 20. Create a method called DuckGoose that accepts an integer. Iterate from 1 to this integer, building a string along the way.
+        // If the current number in the iteration:
+        //   Is divisible by 3, append "Duck" + Environment.NewLine; to the string.
+        //   Is divisible by 5, append "Goose" + Environment.NewLine; to the string.
+        //   Is divisible by both 3 and 5, append "DuckGoose" + Environment.NewLine; to the string.
+        //   Is none of the above, append the number as a string + Environment.NewLine; to the string.
+        /* Example - if the input to this method is 20, the following string should be returned
+         * 1
+         * 2
+         * Duck
+         * 4
+         * Goose
+         * Duck
+         * 7
+         * 8
+         * Duck
+         * Goose
+         * 11
+         * Duck
+         * 13
+         * 14
+         * DuckGoose
+         * 16
+         * 17
+         * Duck
+         * 19
+         * Goose
+         */
+         public static string DuckGoose (int end)
+        {
+            int i = 1;
+            string output = "";
+            while( i <= end )
+            {
+                if (i % 3 == 0)
+                {
+                    output += "Duck";
+                }
+                if (i % 5 == 0)
+                {
+                    output += "Goose";
+                }
+                if (!(i % 3 == 0) && !(i % 5 == 0))
+                {
+                    output += i;
+                }
+                if (i < end)
+                {
+                    output += "\r\n";
+                }
+                i++;
+            }
+            Console.Write(output);
+            return output;
+        }
+
+        // If you've finished all these challenges, sign up for CodeWars.com and try to complete a few C# challenges there!
+    }
+}
